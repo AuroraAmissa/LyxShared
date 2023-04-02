20b65236223b390ebf898572ee555f73625d4447
Alissa Rao <lymia@lymiahugs.com>
2023-04-01 21:30:49 -0700

    Refactor names of Lyx insets.

--- build_common.sh
+++ build_common.sh
@@ -101,6 +101,7 @@ clean_script() {
 }
 
 update_layout() {
+    # Select the correct layout file to include
     layout_file="build/LyxLayout.ins"
     if [[ "$1" == *"Format_Common.lyx"* ]]; then
         layout_file="build/LyxLayoutSystem.ins"
@@ -112,12 +113,25 @@ update_layout() {
         layout_file="build/LyxLayoutSheets.ins"
     fi
     
+    # Rewrite the Lyx file with the new layout
     echo "Updating '$1' with layout '$layout_file'..."
     sed '/\\begin_local_layout/q' "$1" > build/RewriteFile.tmp || exit 1
     cat "$layout_file" >> build/RewriteFile.tmp || exit 1
     echo "\end_local_layout" >> build/RewriteFile.tmp || exit 1
     sed '1,/\\end_local_layout/d' "$1" >> build/RewriteFile.tmp || exit 1
     
+    # Refactor renamed Lyx insets
+    sed -i -e 's/^\\begin_inset Flex IgnoreThis$/\\begin_inset Note Note/' build/RewriteFile.tmp || exit 1
+    sed -i -e 's/^\\begin_inset Flex TwoColumnBox$/\\begin_inset Flex TwoColumns/' build/RewriteFile.tmp || exit 1
+    sed -i -e 's/^\\begin_inset Flex TwoColumnBoxFill$/\\begin_inset Flex TwoColumnsFill/' build/RewriteFile.tmp || exit 1
+    sed -i -e 's/^\\begin_inset Flex LumPartQuote$/\\begin_inset Flex CofDPartQuote/' build/RewriteFile.tmp || exit 1
+    sed -i -e 's/^\\begin_inset Flex LumChapQuote$/\\begin_inset Flex CofDChapterQuote/' build/RewriteFile.tmp || exit 1
+    sed -i -e 's/^\\begin_inset Flex Foldable$/\\begin_inset Flex CollapsableRegion/' build/RewriteFile.tmp || exit 1
+    sed -i -e 's/^\\begin_inset Flex LumIncomplete$/\\begin_inset Flex IncompleteChapter/' build/RewriteFile.tmp || exit 1
+    sed -i -e 's/^\\begin_inset Flex LumIncompleteSection$/\\begin_inset Flex IncompleteSection/' build/RewriteFile.tmp || exit 1
+    sed -i -e 's/^\\begin_inset Flex ContentWarning$/\\begin_inset Flex SidebarContentWarning/' build/RewriteFile.tmp || exit 1
+    
+    # Rewrite the original file if it was changed
     if ! diff -q "$1" build/RewriteFile.tmp &>/dev/null; then
         echo "- Overriding original file..."
         cp build/RewriteFile.tmp "$1" || exit 1
