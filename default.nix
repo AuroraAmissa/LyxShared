3855d77ee85118560e4a249b6d7e502b98de99aa
Lymia Kanokawa <lymia@lymia.moe>
2024-03-17 07:21:46 -0700

    Fix Github Actions script.

--- default.nix
+++ default.nix
@@ -4,7 +4,17 @@ flake-utils.lib.eachDefaultSystem (system:
     let
         pkgs = import nixpkgs {
             inherit system;
-            overlays = [ (import ./overlays.nix) ];
+            overlays = [
+                (self: super: {
+                    lyx = super.lyx.overrideAttrs (oldAttrs: rec {
+                        version = "2.4.0~RC3";
+                        src = super.fetchurl {
+                            url = "https://ftp.lip6.fr/pub/lyx/devel/lyx-2.4/lyx-${version}.tar.xz";
+                            sha256 = "sha256-wwDG/ptMGo3c0v0w7GNvo7zTxCzzxP0bqs9WlTJJuPc=";
+                        };
+                    });
+                })
+            ];
         };
 
         mk_build_script = kind: rec {
